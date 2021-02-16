8c22e7b41ebba93ba2ff46b27682d6558db075d0
ntppro <mr.trongphuongpro@gmail.com>
2021-02-16 21:33:16 +0700

    add socketio

--- client_android.py
+++ client_android.py
@@ -2,6 +2,25 @@ import os
 import signal
 from subprocess import PIPE, Popen
 import time
+import socketio
+
+sio = socketio.Client()
+
+@sio.event
+def connect():
+    print("connected to websocket server.")
+
+@sio.event
+def disconnect():
+    print("disconnect from websocket server.")
+
+@sio.event
+def getinfo():
+    return "Android-Termux"
+
+
+def emitData_callback(res):
+    print(res)
 
 def sigint_callback(sig_num, stack):
     print(f"received: {sig_num}")
@@ -20,13 +39,16 @@ os.close(wf)
 with open(rf, 'r') as file:
     counter = 0
     data = ""
+    total_line = 9
 
     for line in file:
         data += line
         counter += 1
-        if counter == 9:
+        if counter == total_line:
             data = eval(data)["Orientation Sensor"]["values"]
             print(f"yaw: {data[0]} pitch: {data[1]} roll: {data[2]}")
+            sio.emit('data', data, emitData_callback)
+
             data = ""
             counter = 0
 
